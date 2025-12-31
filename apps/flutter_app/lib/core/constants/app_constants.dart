2559415c6be9002351430b6e4b771d3fe6f97792
Manoj Prabhakaran <manojprabhakaran@Manojs-MacBook-Air.local>
2025-12-31 16:08:58 +0400

    basic setup and initial conneciton from database subabase

--- apps/flutter_app/lib/core/constants/app_constants.dart
+++ apps/flutter_app/lib/core/constants/app_constants.dart
@@ -1,4 +1,10 @@
 class AppConstants {
-  // What constants would you need?
+    // App info
+  static const String appName = 'VMS Platform';
   
+  // API
+  static const String baseUrl = 'http://localhost:3000/api/v1';
+  
+  // Storage keys  
+  static const String tokenKey = 'auth_token';
 }
---
+++ apps/flutter_app/lib/core/constants/supabase_config.dart
@@ -0,0 +1,2 @@
+static const String supabaseUrl = 'https://vtpusrpxucshpllclayz.supabase.co';
+static const String supabaseAnonKey = 'sb_publishable_ow6cODyxsvghdCkOcSE0YA_9qXpAMqo';
